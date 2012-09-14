42906f6aa5d8de538ecc8bffbf366808bc992c10
Sam Rawlins <sam.rawlins@gmail.com>
2012-09-14 16:44:45 -0700

    Stamping now works

--- lib/rhubarb/batch_logger.rb
+++ lib/rhubarb/batch_logger.rb
@@ -1,22 +1,23 @@
 class Rhubarb::BatchLogger
   include Log4r
 
-  attr_accessor :log4r_logger
+  attr_accessor :job_stream_file, :log4r_logger
 
-  delegate :info, :error, to: :@log4r_logger
+  delegate :debug, :info, :warn, :error, :fatal, to: :@log4r_logger
 
   def initialize(job_stream)
     raise Rhubarb::MissingBatchHomeError if batch_home.nil?
     raise Rhubarb::InvalidBatchHomeError if not File.exist? batch_home
     batch_home_entries = Dir.new(batch_home).entries.reject {|e| e =~ /\.+|placeholder.txt/}
     raise Rhubarb::EmptyBatchHomeError if batch_home_entries.empty?
-    @job_stream_file = File.join(batch_home, job_stream)
+    @job_stream_file = File.join(batch_home, 'logs', job_stream)
 
     @log4r_logger = Logger.new 'job_stream_logger'
-    @job_stream_file_outputter = FileOutputter.new('fileOutputter', :filename => @job_stream_file)
+    job_stream_file_outputter = FileOutputter.new('fileOutputter', :filename => @job_stream_file)
     @short_formatter = PatternFormatter.new(:pattern => "%d (%l) %m", :date_pattern => "%H:%M:%S")
-    @long_formatter = PatternFormatter.new(:pattern => "%d %m", :date_pattern => "%a, %d %b %Y %H:%M:%S %z")
-    @job_stream_file_outputter.formatter = @short_formatter
+    @long_formatter  = PatternFormatter.new(:pattern => "%d %m", :date_pattern => "%a, %d %b %Y %H:%M:%S %z")
+    job_stream_file_outputter.formatter = @short_formatter
+    @log4r_logger.add(job_stream_file_outputter)
   end
 
   def batch_home
@@ -41,9 +42,22 @@ class Rhubarb::BatchLogger
     info("### #{message}")
   end
 
+  def h4(message)
+    info("#### #{message}")
+  end
+
+  def h5(message)
+    info("##### #{message}")
+  end
+
   def stamp(message)
-    @job_stream_file_outputter.formatter = @long_formatter
-    info message
-    @job_stream_file_outputter.formatter = @short_formatter
+    @log4r_logger.remove(@job_stream_file_outputter)
+    outputter = FileOutputter.new('fileOutputter', :filename => @job_stream_file)
+    outputter.formatter = @long_formatter
+    @log4r_logger.add(outputter)
+    info(message)
+    outputter = FileOutputter.new('fileOutputter', :filename => @job_stream_file)
+    outputter.formatter = @short_formatter
+    @log4r_logger.add(outputter)
   end
 end
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -18,20 +18,20 @@ describe Rhubarb::BatchLogger, '.new' do
   it 'should abandon ship without $BATCH_HOME' do
     Rhubarb::BatchLogger.stub(:batch_home).and_return(nil)
     #Rhubarb::BatchLogger.class_eval { def batch_home; nil; end } # stub it
-    expect { Rhubarb::BatchLogger.new('FOO') }.to raise_error(Rhubarb::MissingBatchHomeError)
+    expect { Rhubarb::BatchLogger.new('foo') }.to raise_error(Rhubarb::MissingBatchHomeError)
   end
 
   it 'should abandon ship with an invalid $BATCH_HOME' do
     batch_home = File.join(@live_dir, 'uaf-fake')
     Rhubarb::BatchLogger.stub(:batch_home).and_return(batch_home)
     #Rhubarb::BatchLogger.class_eval { def batch_home; batch_home; end } # stub it
-    expect { Rhubarb::BatchLogger.new('FOO') }.to raise_error(Rhubarb::InvalidBatchHomeError)
+    expect { Rhubarb::BatchLogger.new('foo') }.to raise_error(Rhubarb::InvalidBatchHomeError)
   end
 
   it 'should abandon ship with an empty $BATCH_HOME directory' do
     batch_home = File.join(@live_dir, 'uaf-tst')
     Rhubarb::BatchLogger.stub(:batch_home).and_return(batch_home)
-    expect { Rhubarb::BatchLogger.new('FOO') }.to raise_error(Rhubarb::EmptyBatchHomeError)
+    expect { Rhubarb::BatchLogger.new('foo') }.to raise_error(Rhubarb::EmptyBatchHomeError)
   end
 
   it 'should abandon ship with empty arguments' do
@@ -41,16 +41,26 @@ describe Rhubarb::BatchLogger, '.new' do
 
   it 'should initialize successfully with one valid argument' do
     Rhubarb::BatchLogger.stub(:batch_home).and_return(@stg_batch_home)
-    expect { Rhubarb::BatchLogger.new('FOO') }.to_not raise_error
+    expect { Rhubarb::BatchLogger.new('foo') }.to_not raise_error
   end
 end
 
 describe Rhubarb::BatchLogger, '#log' do
   before(:all) do
     @live_dir       = File.join(File.dirname(__FILE__), 'live')
+    @canon_dir      = File.join(File.dirname(__FILE__), 'canon')
+    @live_files     = File.join(@live_dir, '*')
+    @canon_files    = File.join(@canon_dir, '*')
     @stg_batch_home = File.join(@live_dir, 'uaf-stg')
+
+    # Delete everything in 'live'
+    FileUtils.rm_rf Dir.glob(@live_files)
+
+    # Copy from 'canon' to 'live'
+    FileUtils.cp_r Dir.glob(@canon_files), @live_dir
+
     Rhubarb::BatchLogger.stub(:batch_home).and_return(@stg_batch_home)
-    @logger = Rhubarb::BatchLogger.new('BAR')
+    @logger = Rhubarb::BatchLogger.new('bar')
   end
 
   it 'should log INFO successfully' do
@@ -61,12 +71,23 @@ describe Rhubarb::BatchLogger, '#log' do
   end
 end
 
-describe Rhubarb::BatchLogger, '#h1' do
+describe Rhubarb::BatchLogger, '#h1, #h2, #h3' do
   before(:all) do
     @live_dir       = File.join(File.dirname(__FILE__), 'live')
+    @canon_dir      = File.join(File.dirname(__FILE__), 'canon')
+    @live_files     = File.join(@live_dir, '*')
+    @canon_files    = File.join(@canon_dir, '*')
     @stg_batch_home = File.join(@live_dir, 'uaf-stg')
+
+    # Delete everything in 'live'
+    FileUtils.rm_rf Dir.glob(@live_files)
+
+    # Copy from 'canon' to 'live'
+    FileUtils.cp_r Dir.glob(@canon_files), @live_dir
+
     Rhubarb::BatchLogger.stub(:batch_home).and_return(@stg_batch_home)
-    @logger = Rhubarb::BatchLogger.new('BAR')
+    @logger = Rhubarb::BatchLogger.new('bar')
+    @message01 = 'Meh Big Fancy Header'
   end
 
   it "should log H1's successfully" do
@@ -82,8 +103,66 @@ describe Rhubarb::BatchLogger, '#h1' do
   end
 
   it "should log H3's successfully" do
-    message = 'Meh Big Fancy Header'
-    @logger.log4r_logger.should_receive(:info).with("### #{message}")
-    @logger.h3(message)
+    @logger.log4r_logger.should_receive(:info).with("### #{@message01}")
+    @logger.h3(@message01)
+  end
+
+  it "should log H4's successfully" do
+    @logger.log4r_logger.should_receive(:info).with("#### #{@message01}")
+    @logger.h4(@message01)
+  end
+
+  it "should log H5's successfully" do
+    @logger.log4r_logger.should_receive(:info).with("##### #{@message01}")
+    @logger.h5(@message01)
+  end
+end
+
+describe Rhubarb::BatchLogger, '#log4r_logger' do
+  before(:all) do
+    @live_dir       = File.join(File.dirname(__FILE__), 'live')
+    @stg_batch_home = File.join(@live_dir, 'uaf-stg')
+    Rhubarb::BatchLogger.stub(:batch_home).and_return(@stg_batch_home)
+    @logger = Rhubarb::BatchLogger.new('foo')
+    @message01 = 'texty text text'
+  end
+
+  it "should actually log successfully" do
+    @logger.info(@message01)
+    lines = File.readlines(@logger.job_stream_file)
+    lines.last.should match /[0-9:]{8} \(INFO\) #{@message01}/
+  end
+
+  it "should actually log successfully" do
+    @logger.warn(@message01)
+    lines = File.readlines(@logger.job_stream_file)
+    lines.last.should match /[0-9:]{8} \(WARN\) #{@message01}/
+  end
+
+  it "should actually log successfully" do
+    @logger.error(@message01)
+    lines = File.readlines(@logger.job_stream_file)
+    lines.last.should match /[0-9:]{8} \(ERROR\) #{@message01}/
+  end
+end
+
+describe Rhubarb::BatchLogger, '#stamp' do
+  before(:all) do
+    @live_dir       = File.join(File.dirname(__FILE__), 'live')
+    @stg_batch_home = File.join(@live_dir, 'uaf-stg')
+    Rhubarb::BatchLogger.stub(:batch_home).and_return(@stg_batch_home)
+    @logger = Rhubarb::BatchLogger.new('foo')
+    @message01 = 'texty text text'
+  end
+
+  it "should stamp successfully" do
+    @logger.log4r_logger.should_receive(:info).with(@message01)
+    @logger.stamp(@message01)
+  end
+
+  it "should actually stamp successfully" do
+    @logger.stamp(@message01)
+    lines = File.readlines(@logger.job_stream_file)
+    lines.last.should match /\w+, \d+ \w+ \d{4} [0-9:]{8} [\-0-9]+ #{@message01}/
   end
 end
