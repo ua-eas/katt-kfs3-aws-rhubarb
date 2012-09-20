d4708df5b3f750a363b2380e2d05b9ea5f771238
Sam Rawlins <sam.rawlins@gmail.com>
2012-09-20 13:19:21 -0700

    Refactored out the Helpers module so cucumber can use it too

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,27 +1,8 @@
 require 'timecop'
 
 require_relative '../lib/rhubarb'
+require_relative 'helpers'
 
-module Helpers
-  def cleanse_live
-    @live_dir       = File.join(File.dirname(__FILE__), 'live')
-    @canon_dir      = File.join(File.dirname(__FILE__), 'canon')
-    @live_files     = File.join(@live_dir, '*')
-    @canon_files    = File.join(@canon_dir, '*')
-    @cfg_batch_home = File.join(@live_dir, 'uaf-cfg')
-    @stg_batch_home = File.join(@live_dir, 'uaf-stg')
-    @trn_batch_home = File.join(@live_dir, 'uaf-trn')
-
-    # Delete everything in 'live'
-    FileUtils.rm_rf Dir.glob(@live_files)
-
-    # Copy from 'canon' to 'live'
-    FileUtils.cp_r Dir.glob(@canon_files), @live_dir
-
-    # For writable tests
-    FileUtils.chmod 0500, File.join(@live_dir, 'uaf-trn', 'control')
-  end
-end
 
 module Matchers
   class IncludeSomethingLike
