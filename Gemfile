c98996b3353658bd87a1e68cc8053018d6a2606e
Sam Rawlins <sam.rawlins@gmail.com>
2012-09-20 13:19:56 -0700

    Created a World so that we pull in Helpers

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'activesupport', '~>3.2'
 gem 'log4r'
 gem 'mail'
+gem 'thor'
 
 group :test do
   gem 'rspec'
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,11 @@
 require 'open4'
 require 'rspec'
+require_relative File.join('..', '..', 'spec', 'helpers')
+
+class RhubarbWorld
+  include Helpers
+end
+
+World do
+  RhubarbWorld.new
+end
