41a90c43546fa40770ef598ad757d2486076d621
Sam Rawlins <sam.rawlins@gmail.com>
2012-11-29 12:24:51 -0700

    Calendaaar

--- lib/rhubarb/calendar.rb
+++ lib/rhubarb/calendar.rb
@@ -11,12 +11,42 @@ class Rhubarb::Calendar
 
     @logger = Rhubarb::Logger.new('calendar')
     debug "Rhubarb::Calendar initialized."
+    @calendar = Calendar.new
   end
 
   def batch_home
     @batch_home ||= Rhubarb.batch_home
   end
 
-  def self.event_from_log_lines(lines)
+  # 2012-10-25 06:50:06,344 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.Job :: Executing job: JobDetail 'unscheduled.processPdpCancelsAndPaidJob':  jobClass: 'org.kuali.kfs.sys.batch.Job isStateful: true isVolatile: false isDurable: true requestsRecovers: false on machine uaz-kc-a50.mosaic.arizona.edu scheduler instance id uaz-kc-a50.mosaic.arizona.edu1351170014622. {status=Scheduled}
+  # 2012-10-25 06:50:06,344 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.Job :: Started processing step: 1=processPdpCancelsAndPaidStep for user <unknown>
+  # 2012-10-25 06:50:06,348 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.Job :: Creating user session for step: processPdpCancelsAndPaidStep=kfs-sys-user
+  # 2012-10-25 06:50:06,348 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.Job :: Executing step: processPdpCancelsAndPaidStep=class org.kuali.kfs.pdp.batch.ProcessPdPCancelsAndPaidStep
+  # 2012-10-25 06:56:03,616 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.Job :: Step processPdpCancelsAndPaidStep of unscheduled.processPdpCancelsAndPaidJob took 5.954466666666666 minutes to complete
+  # 2012-10-25 06:56:03,616 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.Job :: Finished processing step 1: processPdpCancelsAndPaidStep
+  # 2012-10-25 06:56:03,616 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.Job :: Finished executing job: processPdpCancelsAndPaidJob
+  # 2012-10-25 06:56:03,616 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.service.impl.SchedulerServiceImpl :: Updating status of job: processPdpCancelsAndPaidJob=Succeeded
+  # 2012-10-25 06:56:03,623 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.quartz.plugins.history.LoggingTriggerHistoryPlugin :: Trigger unscheduled.processPdpCancelsAndPaidJob completed firing job unscheduled.processPdpCancelsAndPaidJob at  06:56:03 10/25/2012 with resulting trigger instruction code: DELETE TRIGGER
+  def event_from_log_lines(lines)
+    lines = lines.split(/\n+/)
+    event = @calendar.event do
+      dtstart      Rhubarb::Calendar.timestamp_from_line(lines, "Executing job")
+      dtend        Rhubarb::Calendar.timestamp_from_line(lines, "Finished executing job")
+      summary      Rhubarb::Calendar.job_name(lines)
+      description  lines.join('\n')
+    end
+  end
+
+  def self.timestamp_from_line(lines, str)
+    if lines.select { |l| l[str] }.first =~ /^([0-9\- :]+)/
+      return DateTime.parse($1)
+    else
+      nil
+    end
+  end
+
+  def self.job_name(lines)
+    lines.select { |l| l =~ /Executing job: JobDetail 'unscheduled\.([^']*)'/ }.first =~ /Executing job: JobDetail 'unscheduled\.([^']*)'/
+    $1
   end
 end
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -1,7 +1,13 @@
 require_relative 'spec_helper'
 
-describe Rhubarb::Calendar, ".event_from_log_lines" do
-  before(:all) do
+describe Rhubarb::Calendar, "#event_from_log_lines" do
+  include Helpers
+
+  before(:each) do
+    cleanse_live
+    Rhubarb.stub(:batch_home).and_return(@stg_batch_home)
+
+    @cal = Rhubarb::Calendar.new
     lines = <<LOGLINES
 2012-10-25 06:50:06,344 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.Job :: Executing job: JobDetail 'unscheduled.processPdpCancelsAndPaidJob':  jobClass: 'org.kuali.kfs.sys.batch.Job isStateful: true isVolatile: false isDurable: true requestsRecovers: false on machine uaz-kc-a50.mosaic.arizona.edu scheduler instance id uaz-kc-a50.mosaic.arizona.edu1351170014622. {status=Scheduled}
 2012-10-25 06:50:06,344 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.Job :: Started processing step: 1=processPdpCancelsAndPaidStep for user <unknown>
@@ -13,21 +19,18 @@ describe Rhubarb::Calendar, ".event_from_log_lines" do
 2012-10-25 06:56:03,616 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.kuali.kfs.sys.batch.service.impl.SchedulerServiceImpl :: Updating status of job: processPdpCancelsAndPaidJob=Succeeded
 2012-10-25 06:56:03,623 KFS [KFSScheduler_Worker-3] u:/d: INFO  org.quartz.plugins.history.LoggingTriggerHistoryPlugin :: Trigger unscheduled.processPdpCancelsAndPaidJob completed firing job unscheduled.processPdpCancelsAndPaidJob at  06:56:03 10/25/2012 with resulting trigger instruction code: DELETE TRIGGER
 LOGLINES
-    event = Rhubarb::Calendar.event_from_log_lines(lines)
+    @event = @cal.event_from_log_lines(lines)
   end
 
   it "should create an iCalendar event for a single job execution with a correct summary" do
-    pending
-    event.summary.should =~ /processPdpCancelsAndPaidJob/
+    @event.summary.should =~ /processPdpCancelsAndPaidJob/
   end
 
   it "should create an iCalendar event for a single job execution with a correct dtstart" do
-    pending
-    event.dtstart.should == DateTime.new(2012, 10, 25, 6, 50, 06)
+    @event.dtstart.should == DateTime.new(2012, 10, 25, 6, 50, 06)
   end
 
   it "should create an iCalendar event for a single job execution with a correct dtend" do
-    pending
-    event.dtstart.should == DateTime.new(2012, 10, 25, 6, 56, 03)
+    @event.dtend.should   == DateTime.new(2012, 10, 25, 6, 56, 03)
   end
 end
