b9ea76b4d7c5a8899b9ff91c41708de773fd86cc
Josh Shaloo <shaloo@email.arizona.edu>
2013-08-08 13:42:01 -0700

    initial commit of multi output design

--- lib/rhubarb/email/job_stream.rb
+++ lib/rhubarb/email/job_stream.rb
@@ -4,10 +4,25 @@ class Rhubarb::Email::JobStream
   def initialize(config={})
     @name = config['name']
     @outputs = {}
-    @outputs[config['output']['name']] = Rhubarb::Email::Output.new(config['output'])
+    config['outputs'].each |target_name| {
+        @outputs[target_name] = Rhubarb::Email::Output.new(config['outputs'][target_name])
+    }
   end
 
   def deliver(name)
-    @outputs[name].deliver!
+    if (name == 'all' ) {
+        #send all in an each loop
+        @outputs.each | target_name | {
+            @outputs[target_name].deliver!
+        }
+    }
+    elsif ( @outputs[name] ) {
+        # send just the target name
+        @outputs[name].deliver!
+    }
+    else {
+        #thow some exception because the target is undefined.
+    }
+    
   end
 end
