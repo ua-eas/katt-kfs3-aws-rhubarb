2ec25b64296c55dda61f75fe3611167fd1553537
sskinner <sskinner@email.arizona.edu>
2013-08-14 12:09:38 -0700

    Changed to create one Output object per 'target' found in an email YAML config.

--- lib/rhubarb/email/job_stream.rb
+++ lib/rhubarb/email/job_stream.rb
@@ -4,25 +4,34 @@ class Rhubarb::Email::JobStream
   def initialize(config={})
     @name = config['name']
     @outputs = {}
-    config['outputs'].each |target_name| {
-        @outputs[target_name] = Rhubarb::Email::Output.new(config['outputs'][target_name])
-    }
+
+    # target_name = key, the target name
+    # target_entry = value, a nested hash, specific email info for each target
+    config['outputs'].each do |target_name, target_entry|
+        @outputs[target_name] = Rhubarb::Email::Output.new(target_name, target_entry)
+    end
+
   end
 
-  def deliver(name)
-    if (name == 'all' ) {
+
+  def deliver(target_name)
+   
+    if name == 'all'
         #send all in an each loop
-        @outputs.each | target_name | {
+        @outputs.each do |target_name|
             @outputs[target_name].deliver!
-        }
-    }
-    elsif ( @outputs[name] ) {
+        end
+        return 0
+    elsif @outputs[target_name]
         # send just the target name
-        @outputs[name].deliver!
-    }
-    else {
-        #thow some exception because the target is undefined.
-    }
+        @outputs[target_name].deliver!
+        return 0
+    end
+    
+    #throw some exception because the target is undefined.
+    raise "Could not find target with name: " + target_name
+    
     
   end
+
 end
