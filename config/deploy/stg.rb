08c3b084de129657068c51aed04096c174301f6f
Will Austin <quikkian@email.arizona.edu>
2017-05-11 13:51:18 -0700

    UAFAWS-176 - Changing deployment target to localhost for all environments.

--- config/deploy/stg.rb
+++ config/deploy/stg.rb
@@ -1,11 +1,6 @@
 set :stage, :stg
 
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
-role :app, "kualiadm@uaz-kf-a07.mosaic.arizona.edu"
+role :app, %w{ssh-user@localhost:22}
 
 # set :rails_env, :stg
 set :branch, 'staging'
